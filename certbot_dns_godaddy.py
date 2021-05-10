23ace716b89239fbc127e7a794fa71c4ce1f5e09
miigotu <miigotu@gmail.com>
2021-05-10 14:21:54 -0400

    Initial commit (untested, not debugged)
    Signed-off-by: miigotu <miigotu@gmail.com>

---
+++ certbot_dns_godaddy.py
@@ -0,0 +1,76 @@
+"""
+This module defines a certbot plugin to automate the process of completing a
+``dns-01`` challenge (`~acme.challenges.DNS01`) by creating, and subsequently
+removing, TXT records using the godaddy CCP API.
+"""
+
+__all__     = ['Authenticator']
+
+from lexicon.providers import godaddy
+import zope.interface
+
+from certbot import interfaces
+from certbot.plugins import dns_common
+from certbot.plugins import dns_common_lexicon
+
+@zope.interface.implementer(interfaces.IAuthenticator)
+@zope.interface.provider(interfaces.IPluginFactory)
+class Authenticator(dns_common.DNSAuthenticator):
+    """DNS Authenticator for godaddy
+
+    This Authenticator uses the godaddy API to fulfill a dns-01 challenge.
+    """
+
+    description = ('Obtain certificates using a DNS TXT record (if you are '
+                   'using godaddy for DNS).')
+
+    def __init__(self, *args, **kwargs):
+        super(Authenticator, self).__init__(*args, **kwargs)
+        self.credentials = None
+
+    @classmethod
+    def add_parser_arguments(cls, add):
+        super(Authenticator, cls).add_parser_arguments(add)
+        add('credentials', help='godaddy credentials INI file.')
+
+    def more_info(self):
+        return ('This plugin configures a DNS TXT record to respond to a '
+                'dns-01 challenge using the godaddy API.')
+
+    def _setup_credentials(self):
+        self.credentials = self._configure_credentials(
+            'credentials',
+            'godaddy credentials INI file',
+            {
+                'auth-key':  'Key to access the Godaddy API',
+                'auth-secret': 'Secret to access the Godaddy API',
+            }
+        )
+
+    def _perform(self, domain, validation_name, validation):
+        self._get_godaddy_client().add_txt_record(domain, validation_name, validation)
+
+    def _cleanup(self, domain, validation_name, validation):
+        self._get_godaddy_client().del_txt_record(domain, validation_name, validation)
+
+    def _get_godaddy_client(self):
+        credentials = self.credentials.conf
+        return _GodaddyLexiconClient(credentials('auth-key'), credentials('auth-secret'))
+
+
+class _GodaddyLexiconClient(dns_common_lexicon.LexiconClient):
+    """Encapsulates all communication with godaddy via Lexicon."""
+
+    def __init__(self, auth_key, auth_secret):
+        super(_GodaddyLexiconClient, self).__init__()
+        config = dns_common_lexicon.build_lexicon_config('godaddy', {}, {
+            'auth-key': auth_key,
+            'auth-secret': auth_secret,
+        })
+        self.provider = godaddy.Provider(config)
+
+    # called while guessing domain name (going from most specific to tld):
+    def _handle_general_error(self, e, domain_name):
+        if 'Value in field domainname does not match requirements' in str(e):
+            return None
+        return super(_GodaddyLexiconClient, self)._handle_general_error(e, domain_name)
